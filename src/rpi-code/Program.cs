81ba452527d54b5728e9f3b45459807a2a6b303b
bytewizer <58912473+bytewizer@users.noreply.github.com>
2022-03-27 08:39:22 -0700

    General updates

--- src/rpi-code/Program.cs
+++ src/rpi-code/Program.cs
@@ -1,6 +1,7 @@
+using System.Diagnostics;
 using System.Device.Gpio;
 
-Console.WriteLine("Blinking LED.");
+Debug.WriteLine("Blinking LED");
 
 int pin = 13;
 bool ledOn = true;
@@ -8,9 +9,16 @@ bool ledOn = true;
 var controller = new GpioController();
 controller.OpenPin(pin, PinMode.Output);
 
-while (true)
+try
 {
-    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
-    Thread.Sleep(1000);
-    ledOn = !ledOn;
+    while (true)
+    {
+        controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+        Thread.Sleep(1000);
+        ledOn = !ledOn;
+    }
+}
+finally
+{
+    controller.ClosePin(pin);
 }
